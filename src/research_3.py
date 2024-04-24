40f1bed66a367b603b21c4167bdaafe8bd15b4e6
BelkanWar <challenger1501@gmail.com>
2024-04-25 05:39:00 +0800

    2024-4-24 progress

--- src/research_3.py
+++ src/research_3.py
@@ -10,7 +10,7 @@ from config import Config
 interval = Config['scan_setting']['interval']
 
 path_list = []
-for root, folder, files in os.walk(os.path.join(Config['files']['data_folder'], 'lifts')):
+for root, folder, files in os.walk(os.path.join(Config['files']['data_folder'], 'lifts','data')):
     for file in files:
         path_list.append(os.path.join(root, file))
 
@@ -27,22 +27,30 @@ def job(path):
     while ret:
         ret, frame2 = vidcap.read()
         frame_idx += 1
-        if frame_idx % interval == 0:
+        if ret and frame_idx % interval == 0:
             frame2 = frame2.astype(int)
             frame_diff = np.mean((frame2 - frame1)**2, axis=2, dtype=int)[int(h*1/4):int(h*3/4), int(w*1/4):int(w*3/4)]
-            move_pixel_coord = list(np.where(frame_diff>120))
-            move_pixel_coord[0] += int(h*1/4)
-            move_pixel_coord[1] += int(w*1/4)
-            move_pixel_count = len(move_pixel_coord[0])
+            move_pixel = frame_diff > 120
+            edge = cv2.adaptiveThreshold(
+                cv2.cvtColor(frame2.astype(np.uint8), cv2.COLOR_BGR2GRAY), 
+                255, 
+                cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                cv2.THRESH_BINARY_INV, 21, 6
+                )[int(h*1/4):int(h*3/4), int(w*1/4):int(w*3/4)].astype(bool)
 
-            frame_signal_dict['signal'].append(move_pixel_count)
+            move_pixel_coord = list(np.where(move_pixel))
+            move_pixel_count = len(move_pixel_coord[0])
+            edge[tuple(move_pixel_coord)] = False
+            signal = np.count_nonzero(edge, axis=None)
+
+            frame_signal_dict['signal'].append(signal)
             frame_signal_dict['frame_idx'].append(frame_idx)
-            frame_signal_dict['move_pixel_coord'].append(move_pixel_coord)
+            frame_signal_dict['move_pixel_coord'].append((move_pixel_coord[0]+int(h*1/4), move_pixel_coord[1]+int(w*1/4)))
             frame1 = frame2.copy()
     
     q1, q3 = np.quantile(frame_signal_dict['signal'], 0.25), np.quantile(frame_signal_dict['signal'], 0.75)
     frame_signal_dict['move'] = np.zeros(len(frame_signal_dict['signal']), dtype=bool)
-    frame_signal_dict['move'][np.where(np.array(frame_signal_dict['signal'])>q3+1.5*(q3-q1))] = True
+    frame_signal_dict['move'][np.where(np.array(frame_signal_dict['signal'])<q1-1.5*(q3-q1))] = True
 
     vidcap = cv2.VideoCapture(path)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
@@ -68,7 +76,7 @@ def job(path):
 
     out.release()
 
-# with Pool(5) as pool:
-#     pool.map(job, path_list)
+with Pool(5) as pool:
+    pool.map(job, path_list)
 
-job("/media/belkanwar/SATA_CORE/lifts/data/micro travel short sample1.mp4")
+# job("/media/belkanwar/SATA_CORE/lifts/data/micro travel short sample1.mp4")
