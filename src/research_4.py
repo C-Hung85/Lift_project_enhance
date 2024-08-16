be93ef1b9bdfc22899ca9d80f677450f1e6f565c
BelkanWar <challenger1501@gmail.com>
2024-08-17 02:41:56 +0800

    float --> booleam

--- src/research_4.py
+++ src/research_4.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from multiprocessing import Pool
+from scipy.sparse import lil_array
 from sklearn.decomposition import TruncatedSVD, NMF
 from sklearn.preprocessing import StandardScaler
 from config import Config
@@ -31,19 +32,24 @@ def job(path):
     
     h, w = frame1.shape[:2]
     h_roi, w_roi = [[int(h*1/4), int(h*3/4)], [int(w*1/4), int(w*3/4)]]
-    frames_array = []
-
-    while ret:
-        ret, frame = vidcap.read()
-        if ret:
-            frame = np.mean(frame[h_roi[0]:h_roi[1], w_roi[0]:w_roi[1]].astype(int), axis=2)
-            frames_array.append(frame.reshape(-1))
+    move_pixel_coord_list = []
     
-    frames_array = np.array(frames_array)
-    decomposer = TruncatedSVD(n_components = N_DECOMPOSITION)
-    scaler = StandardScaler(with_std = False)
-    frames_array = scaler.fit_transform(frames_array)
-    weight = decomposer.fit_transform(frames_array)
+    while ret:
+        ret, frame2 = vidcap.read()
+        frame_idx += 1
+        if ret and frame_idx % interval == 0:
+            frame2 = frame2.astype(int)
+            frame_diff = np.mean((frame2 - frame1)**2, axis=2, dtype=int)[h_roi[0]:h_roi[1], w_roi[0]:w_roi[1]]
+            move_pixel_coord = np.where((frame_diff > 120).reshape(-1))
+            move_pixel_coord_list.append(move_pixel_coord)
+        
+    move_pixel_array = lil_array((len(move_pixel_coord_list), (h_roi[1]-h_roi[0])*(w_roi[1]-w_roi[0])), dtype=float)
+
+    for idx, move_pixel_coord in enumerate(move_pixel_coord_list):
+        move_pixel_array[idx, move_pixel_coord] = 1
+
+    decomposer = NMF(n_components = N_DECOMPOSITION)
+    weight = decomposer.fit_transform(move_pixel_array)
     feature = decomposer.components_
 
     for i in range(10):
