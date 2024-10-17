38e6c5835df248b065fab5b39586eeea5dac87b3
BelkanWar <challenger1501@gmail.com>
2024-10-18 01:02:32 +0800

    progress

--- src/research_7.py
+++ src/research_7.py
@@ -1,5 +1,6 @@
 '''reference
 https://blog.csdn.net/weixin_43151193/article/details/125222481
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html
 '''
 
 import os
@@ -8,9 +9,11 @@ os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append("src/")
 import cv2
 import numpy as np
+from sklearn.metrics import pairwise_distances
 from multiprocessing import Pool
 from config import Config
 
+orb = cv2.ORB_create(nfeatures=100)
 interval = Config['scan_setting']['interval']
 display = True
 
@@ -19,102 +22,45 @@ for root, folder, files in os.walk(os.path.join(Config['files']['data_folder'], 
     for file in files:
         path_list.append(os.path.join(root, file))
 
+path = "/media/belkanwar/SATA_CORE/lifts/data/micro travel short sample1.mp4"
 
 def job(path):
     vidcap = cv2.VideoCapture(path)
-    ret, frame1 = vidcap.read()
+    ret, frame = vidcap.read()
+    kp1, des1 = orb.detectAndCompute(frame, None)
+    frame = cv2.drawKeypoints(frame, kp1, None, color=(0, 255, 0), flags=0)
     frame_idx = 0
-    frame1 = frame1.astype(int)
-    
-    frame_signal_dict = {field:[] for field in ['frame_idx', 'move_signal', 'block_signal', 'move_pixel_coord']}
-    h, w = frame1.shape[:2]
+    h, w = frame.shape[:2]
 
-    while ret:
-        ret, frame2 = vidcap.read()
-        frame_idx += 1
-        if ret and frame_idx % interval == 0:
-            frame2 = frame2.astype(int)
-            frame_diff = np.mean((frame2 - frame1)**2, axis=2, dtype=int)[int(h*1/4):int(h*3/4), int(w*1/4):int(w*3/4)]
-            move_pixel = frame_diff > 120
-            edge = cv2.adaptiveThreshold(
-                cv2.cvtColor(frame2.astype(np.uint8)[int(h*1/4):int(h*3/4), int(w*1/4):int(w*3/4)], cv2.COLOR_BGR2GRAY), 
-                255, 
-                cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
-                cv2.THRESH_BINARY_INV, 21, 6).astype(bool)
-
-            ''' discrimination logics
-            blocked frames: the count of edge pixels suddently drop
-            moving frames: the count of edge pixels - moving pixel suddently drop
-            '''
-            move_pixel_coord = list(np.where(move_pixel))
-            block_signal = np.count_nonzero(edge)
-            edge[tuple(move_pixel_coord)] = False
-            move_signal = np.count_nonzero(edge)
-            
-            frame_signal_dict['move_signal'].append(move_signal)
-            frame_signal_dict['block_signal'].append(block_signal)
-            frame_signal_dict['frame_idx'].append(frame_idx)
-            frame_signal_dict['move_pixel_coord'].append((move_pixel_coord[0]+int(h*1/4), move_pixel_coord[1]+int(w*1/4)))
-            frame1 = frame2.copy()
-    
-    # (edge pixel - moving pixel) suddenly dropped --> Lift moving
-    q1, q3 = np.quantile(frame_signal_dict['move_signal'], 0.25), np.quantile(frame_signal_dict['move_signal'], 0.75)
-    frame_signal_dict['move'] = np.zeros(len(frame_signal_dict['move_signal']), dtype=bool)
-    frame_signal_dict['move'][np.where(np.array(frame_signal_dict['move_signal']) < q1-1.5*(q3-q1))] = True
-
-    # edge pixel suddenly dropped --> image blocked
-    q1, q3 = np.quantile(frame_signal_dict['block_signal'], 0.25), np.quantile(frame_signal_dict['block_signal'], 0.75)
-    frame_signal_dict['block'] = np.zeros(len(frame_signal_dict['block_signal']), dtype=bool)
-    frame_signal_dict['block'][np.where(np.array(frame_signal_dict['block_signal']) < q1-5*(q3-q1))] = True
-
-    vidcap = cv2.VideoCapture(path)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(path.replace("data", "result"), fourcc, vidcap.get(cv2.CAP_PROP_FPS)/interval, (w, h))
+    out.write(frame)
 
-    if display:
-        frame_signal_dict['move_signal'] = np.max(frame_signal_dict['move_signal']) - np.array(frame_signal_dict['move_signal'])
-        frame_signal_dict['block_signal'] = np.max(frame_signal_dict['block_signal']) - np.array(frame_signal_dict['block_signal'])
-        max_move_signal = np.max(frame_signal_dict['move_signal'])
-        max_block_signal = np.max(frame_signal_dict['block_signal'])
-
-    for frame_idx, move_signal, move, block_signal, block, move_pixel_coord in zip(
-        frame_signal_dict['frame_idx'], 
-        frame_signal_dict['move_signal'], 
-        frame_signal_dict['move'],
-        frame_signal_dict['block_signal'],
-        frame_signal_dict['block'],
-        frame_signal_dict['move_pixel_coord']):
-
-        if display:
-            move_signal_display = round(move_signal/max_move_signal, 3)
-            block_signal_display = round(block_signal/max_block_signal, 3)
-
-        vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+    while ret:
         ret, frame = vidcap.read()
+        frame_idx += 1
 
-        if ret:
-            frame[tuple(move_pixel_coord)] = [0, 255, 0]
+        if ret and frame_idx % interval == 0:
+            kp2, des2 = orb.detectAndCompute(frame, None)
+            frame = cv2.drawKeypoints(frame, kp2, None, color=(0, 255, 0), flags=0)
 
-            if block:
-                color = [0, 255, 255]
-                condition = "blocked"
-            else:
-                if move:
-                    color = [0, 255, 0]
-                    condition = "moving"
-                else:
-                    color = [0, 0, 255]
-                    condition = "static"
-            
-            cv2.putText(frame, f"move signal: {move_signal_display} | block signal: {block_signal_display}", (10, h-10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
-            cv2.putText(frame, condition, (w-200, h-10), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+            distance_array = pairwise_distances(des2, des1)
+            closest_kp1_idx = np.argmin(distance_array, axis=1)
+
+            for kp2_idx in range(distance_array.shape[0]):
+                kp1_idx = closest_kp1_idx[kp2_idx]
+                kp1_coord = np.array(kp1[kp1_idx].pt, dtype=int)
+                kp2_coord = np.array(kp2[kp2_idx].pt, dtype=int)
+            cv2.line(frame, kp1_coord, kp2_coord, [0, 0, 255], 2)
 
             out.write(frame)
-
+            kp1 = kp2
+            des1 = des2
+            
     out.release()
     print(f"complete: {path}")
 
-with Pool(5) as pool:
-    pool.map(job, path_list)
+# with Pool(5) as pool:
+#     pool.map(job, path_list)
 
-# job("/media/belkanwar/SATA_CORE/lifts/data/micro travel short sample1.mp4")
+job(path)
