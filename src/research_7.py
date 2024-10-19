c725ff6f22d45e46a9dbd22f6b3d031c7ad799e9
BelkanWar <challenger1501@gmail.com>
2024-10-19 22:19:21 +0800

    big break through

--- src/research_7.py
+++ src/research_7.py
@@ -7,13 +7,18 @@ import os
 import sys
 os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append("src/")
+import warnings
 import cv2
 import numpy as np
-from sklearn.metrics import pairwise_distances
+from sklearn.cluster import KMeans
 from multiprocessing import Pool
 from config import Config
 
+warnings.filterwarnings('ignore') 
+
 orb = cv2.ORB_create(nfeatures=100)
+bf_matcher = cv2.BFMatcher(normType=cv2.NORM_HAMMING, crossCheck=True)
+cluster = KMeans(n_clusters=2)
 interval = Config['scan_setting']['interval']
 display = True
 
@@ -42,16 +47,35 @@ def job(path):
 
         if ret and frame_idx % interval == 0:
             kp2, des2 = orb.detectAndCompute(frame, None)
-            frame = cv2.drawKeypoints(frame, kp2, None, color=(0, 255, 0), flags=0)
+            
+            matches = bf_matcher.match(des2, des1)
+            kp_idx_dist_array = []
 
-            distance_array = pairwise_distances(des2, des1)
-            closest_kp1_idx = np.argmin(distance_array, axis=1)
-
-            for kp2_idx in range(distance_array.shape[0]):
-                kp1_idx = closest_kp1_idx[kp2_idx]
+            for match_info in matches:
+                kp1_idx = match_info.trainIdx
+                kp2_idx = match_info.queryIdx
                 kp1_coord = np.array(kp1[kp1_idx].pt, dtype=int)
                 kp2_coord = np.array(kp2[kp2_idx].pt, dtype=int)
-            cv2.line(frame, kp1_coord, kp2_coord, [0, 0, 255], 2)
+                kp_idx_dist_array.append([kp2_idx, kp1_idx, np.sqrt(np.sum((kp1_coord - kp2_coord)**2))])
+
+            kp_idx_dist_array = np.array(kp_idx_dist_array)
+            distance_q1, distance_q3 = np.quantile(kp_idx_dist_array[:,2], 0.25), np.quantile(kp_idx_dist_array[:,2], 0.75)
+            valid_kp_idx_dist_array = kp_idx_dist_array[np.where(kp_idx_dist_array[:,2] <= distance_q3+1.5*(distance_q3-distance_q1))[0]]
+            valid_kp_list = [kp2[i] for i in valid_kp_idx_dist_array[:, 0].astype(int)]
+
+            for kp2_idx, kp1_idx, distance in valid_kp_idx_dist_array:
+                cv2.line(frame, np.array(kp1[int(kp1_idx)].pt, dtype=int), np.array(kp2[int(kp2_idx)].pt, dtype=int), [0, 0, 255], 2)
+            frame = cv2.drawKeypoints(frame, valid_kp_list, None, color=(0, 255, 0), flags=0)
+
+            group_idx_array = cluster.fit_predict(valid_kp_idx_dist_array[:,2].reshape(-1,1))
+            if len(set(group_idx_array)) > 1:
+                group0_move_distance = np.mean(valid_kp_idx_dist_array[np.where(group_idx_array==0)[0], 2])
+                group1_move_distance = np.mean(valid_kp_idx_dist_array[np.where(group_idx_array==1)[0], 2])
+                distance_diff = abs(group0_move_distance - group1_move_distance)
+            else:
+                distance_diff = 0
+
+            cv2.putText(frame, str(distance_diff), (w-200, h-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
             out.write(frame)
             kp1 = kp2
