a9b5c8bb0084ded6da7f4fea446ecea840afff2f
BelkanWar <challenger1501@gmail.com>
2024-11-04 05:44:51 +0800

    try to build a interactive streamlit app interact with snapshots of videos

---
+++ src/GUI.py
@@ -0,0 +1,9 @@
+# https://image-coordinates.streamlit.app/dynamic_update
+# https://docs.streamlit.io/develop/api-reference/widgets
+import streamlit as st
+from PIL import Image, ImageDraw
+from streamlit_image_coordinates import streamlit_image_coordinates
+
+
+# def main():
+    # st.sidebar
